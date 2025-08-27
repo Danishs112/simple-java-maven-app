a8d83f838a14f281a1469dde85db3c117f8bcae2
Danish <42249231+Danishs112@users.noreply.github.com>
2025-08-27 15:22:39 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,25 +2,28 @@
 
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
+echo "Jenkins's local Maven repository (and the \"maven-repository\" Docker data volume)."
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
 echo 'The following command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+echo 'within <project/> of your Java/Maven project\'s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | sed 's/\x1b\[[0-9;]*m//g' | tr -d '\r\n')
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | sed 's/\x1b\[[0-9;]*m//g' | tr -d '\r\n')
 set +x
+
+echo "Extracted project name: $NAME"
+echo "Extracted project version: $VERSION"
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar "target/${NAME}-${VERSION}.jar"
+set +x
